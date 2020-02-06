29ab6f054342ee5f7d9223384ca9532c0671806e
Psharon-7 <zhilinli@usc.edu>
2020-02-06 15:57:54 -0800

    upload

--- application/views/pnscreening_form.php
+++ application/views/pnscreening_form.php
@@ -1,5 +1,14 @@
-<?php $this->load->view('templates/header'); ?>
+<?php $this->load->view('templates/header_form'); ?>
 
-<h1>Page1</h1>
+<form>
+  <div class="form-group"> <!-- left unspecified, .bmd-form-group will be automatically added (inspect the code) -->
+    <label for="formGroupExampleInput" class="bmd-label-floating">Example label</label>
+    <input type="text" class="form-control" id="formGroupExampleInput">
+  </div>
+  <div class="form-group bmd-form-group"> <!-- manually specified --> 
+    <label for="formGroupExampleInput2" class="bmd-label-floating">Another label</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2">
+  </div>
+</form>
 
 <?php $this->load->view('templates/footer'); ?>
